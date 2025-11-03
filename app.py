15377d511d237ebc3f4cdfeeeb3b94f659ba6867
yogini1114 <yogini110905@gmail.com>
2025-11-03 14:55:37 +0530

    Update app.py
    Replaced tfidf with cv for vectorization in prediction.

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ pickle.dump(cv, open('vectorizer.pkl', 'wb'))
 print("Model and vectorizer saved successfully!")
 
 # Streamlit UI
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
+cv = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
 st.title("SMS Spam Classifier")
@@ -71,7 +71,8 @@ input_sms = st.text_input("Enter the message")
 
 if st.button('Predict'):
     transformed_sms = transform_text(input_sms)
-    vector_input = tfidf.transform([" ".join(transformed_sms)])
+    vector_input = cv.transform([transformed_sms])
+
     result = model.predict(vector_input)[0]
     if result == 1:
         st.header("Spam")
